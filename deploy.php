bc06a3788bba3b5ea1df43b64526deb2679a9c0f
hirai <y-hirai@r-up.jp>
2018-11-22 15:12:09 +0900

    add deployer

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php
+namespace Deployer;
+
+require 'recipe/composer.php';
+// デプロイ先
+server('production', '13.112.171.83', 22)
+    ->user('ec2-user')
+    ->identityFile()
+    ->forwardAgent()
+    ->env('deploy_path', '~/test/');
+set('ssh_type', 'native');
+// リポジトリ
+set('repository', 'https://github.com/arexxxx/test.git');
